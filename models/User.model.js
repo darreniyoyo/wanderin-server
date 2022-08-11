b96c72efa859958c8cedd339fd00a9b0b5c34897
Darren Iyoyo <darreniyoyo@gmail.com>
2022-08-11 14:31:30 +0200

    username needs to be unique instead of email

--- models/User.model.js
+++ models/User.model.js
@@ -3,9 +3,9 @@ const { Schema, model } = mongoose;
 
 
 const userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, uniqe: true },
 });
 
 module.exports = model("User", userSchema);
